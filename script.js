4235e43c1b97b4fa607fd747b7912af0c0a34edb
KPart1111 <keith.chris.p@gmail.com>
2024-11-22 20:30:00 -0500

    added js and reset

---
+++ script.js
@@ -0,0 +1,10 @@
+// declare!
+let toggle = document.querySelector('.menu-toggle');
+let menu = document.querySelector('.menu');
+
+// menu toggle function
+function toggleMenu() {
+     menu.classList.toggle('show');
+}
+toggle.addEventListener('click',toggleMenu);
+
